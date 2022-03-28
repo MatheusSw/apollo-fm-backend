dbe85098b5c9d6d62bf62bd2fc0b0f2e5599b07a
Matheus <anyonebr1@gmail.com>
2022-03-28 18:30:47 -0300

    Added dumb healthcheck

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('foo', fn() => 'bar');
 
 Route::middleware('auth:sanctum')->prefix('user')->group(function () {
     Route::get('/', [UserController::class, 'show']);
